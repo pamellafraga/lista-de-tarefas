8f98fa0b08da31c816cec88c68fdc2eedd587d62
Pamella Fraga <pamellafrag@gmail.com>
2025-08-04 19:35:36 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function adicionarTarefa() {
+  const tarefa = document.getElementById("tarefa").value;
+  if (tarefa === "") return;
+  const li = document.createElement("li");
+  li.textContent = tarefa;
+  document.getElementById("lista").appendChild(li);
+  document.getElementById("tarefa").value = "";
+}
